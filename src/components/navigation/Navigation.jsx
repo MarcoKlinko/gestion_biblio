53e77b6a067d8e3a1c17fce4d7d0b73ec3041621
Duhamel <marcoblaweky@gmail.com>
2024-06-21 20:18:42 +0300

    Create Book Page

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -4,7 +4,7 @@ import style from "./style.module.css";
 
 const Navigation = () => {
     return <nav className="navbar navbar-expand-lg bg-body-tertiary">
-    <div className="container-fluid">
+    <div className="container">
       <Link className="navbar-brand" to="/">LOGO</Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
@@ -22,7 +22,7 @@ const Navigation = () => {
             <Link className="nav-link" to="/login">Se connecter</Link>
         </li>
         <form className="d-flex" role="search">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+            <input className="form-control" type="search" placeholder="Search" aria-label="Search"/>
             <button className="btn btn-outline-success" type="submit">Search</button>
         </form>
       </div>
--- src/pages/books/BookPage.jsx
+++ src/pages/books/BookPage.jsx
@@ -1,8 +1,42 @@
 import React from "react";
+import style from "./style.module.css";
+
+const bookList = [
+    { id: 1, name: "book1", imgUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7MmZsXpsy4zN3ezEZ4O8JfgwnthKeMogyRA&s"},
+    { id: 2, name: "book2", imgUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7MmZsXpsy4zN3ezEZ4O8JfgwnthKeMogyRA&s"},
+    { id: 3, name: "book3", imgUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7MmZsXpsy4zN3ezEZ4O8JfgwnthKeMogyRA&s"},
+    { id: 4, name: "book4", imgUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7MmZsXpsy4zN3ezEZ4O8JfgwnthKeMogyRA&s"},
+    { id: 5, name: "book5", imgUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7MmZsXpsy4zN3ezEZ4O8JfgwnthKeMogyRA&s"},
+    { id: 5, name: "book5", imgUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7MmZsXpsy4zN3ezEZ4O8JfgwnthKeMogyRA&s"},
+    { id: 5, name: "book5", imgUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7MmZsXpsy4zN3ezEZ4O8JfgwnthKeMogyRA&s"},
+    { id: 5, name: "book5", imgUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7MmZsXpsy4zN3ezEZ4O8JfgwnthKeMogyRA&s"},
+    { id: 5, name: "book5", imgUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7MmZsXpsy4zN3ezEZ4O8JfgwnthKeMogyRA&s"},
+    { id: 5, name: "book5", imgUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7MmZsXpsy4zN3ezEZ4O8JfgwnthKeMogyRA&s"},
+]
 
 const BookPage = () => {
     
-    return <div>BookPage</div>
+    return <div classNameName="container-fluid">
+        <div className="row gap-1 p-5">
+        {
+            bookList.map( book => 
+                <div className={`col-3 ${style.card__wrapper} my-2`}>
+                    <div className="card">
+                        <img src={book.imgUrl} className="card-img-top image-fluid" alt={book.name} />
+                        <div className="card-body">
+                            <h5 className="card-title text-center">{book.name}</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        <div className="card-body">
+                            <a href="#" className="card-link">Card link</a>
+                            <a href="#" className="card-link">Another link</a>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+         </div>
+    </div>
 }
 
 export default BookPage;
